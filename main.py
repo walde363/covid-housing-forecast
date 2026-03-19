a7c552f094bae144f691bf958c8f28014d03e44a
walde363 <ramoswaldemar082@gmail.com>
2026-03-19 12:53:03 -0400

    Changed model function parameters to accept selected cols instead of unwanted cols.

--- main.py
+++ main.py
@@ -4,17 +4,8 @@ import pandas as pd
 def main():
     data = pd.read_csv("data/processed/processed_data_low.csv")
     
-    cols_to_drop = [
-        "RegionID",
-        "Month",
-        "StateName",
-        "NewConSales",
-        "ZORDI_Condo",
-        "ZORDI_MFR",
-        "ZORDI_SFR",
-        "DaysToClose",
-    ]
-    result = rf_xgb_models("MarketTemp", data, cols_to_drop)
+    selected_cols = ["SizeRank", "RegionName", "RegionType", "MarketTemp", "date", "ZHVI_Tier", "ZORDI_All"]
+    result = rf_xgb_models("ZHVI_Tier", data, selected_cols)
     
     print(result)
 
